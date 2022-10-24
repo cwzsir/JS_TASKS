70cecf401cdf70feacdb975d3f9b1a35aea03aa5
syimyk isakov <syimyk.isakov55@gmail.com>
2022-10-24 10:34:17 +0600

    fix: shorting the code

--- 24-10-task.02/script.js
+++ 24-10-task.02/script.js
@@ -1,7 +1,7 @@
-let name = 'Syimyk';
+let name = 'Syimyk',
+    age = '21',
+    str1 = 'САЛАМ,',
+    str2 = ' БИШКЕК!';
 alert('Привет '+name);
-let age = '21';
 alert('Мне '+age+' лет');
-let str1 = 'САЛАМ,';
-let str2 = ' БИШКЕК!';
 alert(str1+str2);
