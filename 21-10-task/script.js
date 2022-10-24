8f8b200627c85185ffe9158122ae3eecb80c8d2d
syimyk isakov <syimyk.isakov55@gmail.com>
2022-10-24 09:39:10 +0600

    init: start

---
+++ 21-10-task/script.js
@@ -0,0 +1,4 @@
+let name1 = 'Syimyk';
+let name2 = 'Isakov';
+name2 = name1;
+alert(name2);
---
+++ 24-10-task/script.js
@@ -0,0 +1,6 @@
+let numberOne = 0;
+let number_two = 1;
+let NumberThree = 2;
+alert(numberOne);
+alert(number_two);
+alert(NumberThree);
