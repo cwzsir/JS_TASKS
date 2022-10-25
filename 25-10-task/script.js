b78ff0fe5199c386dc59b41c7e5b73d6e77d394b
syimyk isakov <syimyk.isakov55@gmail.com>
2022-10-25 10:48:32 +0600

    feat:added new file

---
+++ 25-10-task/script.js
@@ -0,0 +1,17 @@
+let a = 17;
+let b = 10;
+let minus = a - b;
+let c = minus;
+let d = 7;
+let result = c + d;
+console.log(result);
+
+let h = 'час';
+let m = 'минута';
+let s = 'секунда';
+console.log(`${h}:${m}:${s}`)
+
+var num = 10;
+let num1 = num++;
+let num2 = num++;
+alert(num);
