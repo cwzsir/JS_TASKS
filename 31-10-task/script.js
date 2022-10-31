e5d63249c9cc8aa4c04f3613aa71e839d99cd83c
syimyk isakov <syimyk.isakov55@gmail.com>
2022-10-31 11:01:59 +0600

    feat: added new file

---
+++ 31-10-task/script.js
@@ -0,0 +1,20 @@
+const userSalaries = {
+    Ella: '30000 som',
+    Sophia: '40000 som',
+    Ellie: '50000 som'
+}
+alert(userSalaries.Ella);
+alert(userSalaries.Sophia);
+
+const plus = {
+    a: 5,
+    b: 6,
+    c: 10
+}
+
+alert(plus.a+plus.b+plus.c);
+
+let data = 5;
+let number;
+number = data ? 3 : 5;
+console.log(number);
