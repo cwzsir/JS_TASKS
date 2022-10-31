69fafeea6ccaf9e5b6f96fddb305273a48fb6d14
syimyk isakov <syimyk.isakov55@gmail.com>
2022-10-31 09:04:20 +0600

    fix: script.js

--- 28-10-task/script.js
+++ 28-10-task/script.js
@@ -1,19 +1,19 @@
-let x = prompt('Введите число от 1 до 10');
-if(x === '7'){
+let x = +prompt('Введите число от 1 до 10');
+if(x === 7){
     alert('Верно');
-} else if(x !== '7'){
+} else{
     alert('Неверно');
 }
 
-let a = prompt();
-let b = prompt();
+let a = +prompt();
+let b = +prompt();
 if(a <= 1 && b >= 3){
-    alert(Number(a) + Number(b));
+    alert(a + b);
 } else {
-    alert(Number(a) - Number(b));
+    alert(a - b);
 }
 
-let time = prompt('Введите число от 0 до 59');
+let time = +prompt('Введите число от 0 до 59');
 if(time >= 0 && time < 15){
     alert('первая четверть');
 } else if (time >= 15 && time < 29){
@@ -22,6 +22,6 @@ if(time >= 0 && time < 15){
     alert('третья четверть');
 } else if (time >= 45 && time <= 59){
     alert('четвертая четверть');
-} else if (time !== 0 > 59){
+} else{
     alert('Введите число от 0 до 59');
 }
